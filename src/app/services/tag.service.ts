3bf664e04f875f02ff82fecf183d57566592fe10
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2019-01-05 23:07:11 +0000

    Ecriture de TagService

--- src/app/services/tag.service.ts
+++ src/app/services/tag.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagService {
 
-  constructor() { }
+  serviceURl = 'http://localhost:3000/tagList';
+  tags: any[];
+  tagSubject = new Subject<any[]>();
+
+  constructor(private http: HttpClient) { }
+
+  emitTags() {
+    this.tagSubject.next(this.tags.slice());
+  }
+
+  getTags() {
+    const url = this.serviceURl;
+    this.http.get<any[]>(url).subscribe(
+      (response) => {
+        this.tags = response;
+        this.emitTags();
+      },
+      (error) => {
+        console.log('Erreur : ' + error);
+      }
+    );
+  }
 }
