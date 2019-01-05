46106024c9e0926144c1131482880bf1af24ed13
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2019-01-05 19:08:27 +0000

    Création du model category

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  private serviceURl = 'http://localhost:3000/categories';
+
+  constructor(private http: Http) { }
+
+  getCategories() {
+
+
+  }
 }
