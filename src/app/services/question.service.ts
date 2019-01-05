58dbddcfea56374eeaa7752fb600b63156e5da31
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2019-01-05 23:03:01 +0000

    Ecriture de questionService

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { Question } from '../models/question.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class QuestionService {
 
-  constructor() { }
+  serviceURl = 'http://localhost:3000/questions';
+  questions: Question[];
+  questionSubject = new Subject<Question[]>();
+
+  constructor(private http: HttpClient) { }
+
+  emitQuestions() {
+    this.questionSubject.next(this.questions.slice());
+  }
+
+  getQuestions() {
+    const url = this.serviceURl;
+    this.http.get<Question[]>(url).subscribe(
+      (response) => {
+        this.questions = response;
+        this.emitQuestions();
+      },
+      (error) => {
+        console.log('Erreur : ' + error);
+      }
+    );
+  }
 }
