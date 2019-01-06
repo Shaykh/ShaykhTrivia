6ab39eae94fe53d1dfd828bea4c28268b481bf3d
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2019-01-06 00:04:54 +0000

    Ajout des services aux providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CategoryService } from './services/category.service';
+import { QuestionService } from './services/question.service';
+import { TagService } from './services/tag.service';
+
 import { AppComponent } from './components/app.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { QuestionsComponent } from './components/questions/questions.component';
@@ -16,7 +20,11 @@ import { TagsComponent } from './components/tags/tags.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CategoryService,
+    QuestionService,
+    TagService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
