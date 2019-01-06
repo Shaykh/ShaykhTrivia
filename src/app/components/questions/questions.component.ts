f77d60b8050560dc7190dd88b74fe202d4bb1d50
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2019-01-06 11:26:07 +0000

    Ajout de questionComponent

--- src/app/components/questions/questions.component.ts
+++ src/app/components/questions/questions.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { QuestionService } from './../../services/question.service';
+import { Question } from './../../models/question.model';
 
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css']
 })
-export class QuestionsComponent implements OnInit {
+export class QuestionsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  questions: Question[];
+  questionSubscription: Subscription;
+
+  constructor(private questionService: QuestionService) { }
 
   ngOnInit() {
+    this.questionSubscription = this.questionService.questionSubject
+      .subscribe(
+        (questions: Question[]) => {
+          this.questions = questions;
+        }
+      );
+      this.questionService.emitQuestions();
   }
 
+  ngOnDestroy() {
+    this.questionSubscription.unsubscribe();
+  }
 }
